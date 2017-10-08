4eefd5b6da0c7d543bf9bb0cabd301ab298bb5f3
Andre <elij.richards22@gmail.com>
2017-10-08 12:42:56 -0400

    obile views done

--- js.js
+++ js.js
@@ -9,6 +9,7 @@
         $('.nameDisplay').fadeIn(300)
         $('.menuBtn').removeClass('selected')
         $('#homeBtn').addClass('selected')
+        hamToggle()
     })
 
     $('#aboutBtn').click(() => {
@@ -19,6 +20,7 @@
         $('.aboutSection').fadeIn(300)
         $('.menuBtn').removeClass('selected')
         $('#aboutBtn').addClass('selected')
+        hamToggle()
     })
 
     $('#projectBtn').click(() => {
@@ -29,6 +31,7 @@
         $('.projectSection').fadeIn(300)
         $('.menuBtn').removeClass('selected')
         $('#projectBtn').addClass('selected')
+        hamToggle()
     })
 
     $('#contactBtn').click(() => {
@@ -39,19 +42,20 @@
         $('.contactSection').fadeIn(400)
         $('.menuBtn').removeClass('selected')
         $('#contactBtn').addClass('selected')
+        hamToggle()
     })
 
     $('.arrowEmail').mouseenter(() => {
         $('.contactArrow').animate({
             marginLeft: "6px"
-        }, 300, 'linear', () => {
+        }, 300, 'swing', () => {
             $('.contactArrow').animate({
                 marginLeft: "6px"
             }, 300)
         })
         $('.arrowEmail h2').animate({
             marginLeft: "-6px"
-        }, 300, 'linear', () => {
+        }, 300, 'swing', () => {
             $('.arrowEmail h2').animate({
                 marginLeft: "-6px"
             }, 300)
@@ -66,4 +70,14 @@
             marginLeft: "6px"
         }, 200)
     })
+    $('.hamburger').click(() => {
+        hamToggle()
+    })
+}
+
+const hamToggle = () => {
+    if ($(window).width() < 741) {
+        $('#sideNav').toggle('slide')
+        $('.hamburger').toggleClass("change")
+    }
 }
